e7e734d519b01e5d1668a8c80527cc7da053dd47
kojimakunren <kojimakunren@gmail.com>
2023-11-20 20:25:02 +0900

    202311202025

--- Assets/Kojima/Scripts/NameText.cs
+++ Assets/Kojima/Scripts/NameText.cs
@@ -5,20 +5,19 @@ using UnityEngine.UI;
 
 public class NameText : MonoBehaviour
 {
-    public Player player;
+    [SerializeField] private Player player; //プレイヤー格納用
 
-    string name;
-    Text nameText;
+    string name; //プレイヤー名格納用
+    Text nameText; //ネームテキスト格納用
 
     void Start()
     {
-        nameText = this.GetComponent<Text>();
-
+        nameText = this.GetComponent<Text>(); //ネームテキストのテキストコンポーネントを取得
     }
 
     void Update()
     {
-        name = player.GetName();
-        nameText.text = name;
+        name = player.GetName(); //プレイヤー名を取得
+        nameText.text = name; //プレイヤー名を表示
     }
 }
